986b638c69f2f6b9e544ceaa7f19e163d9331bb4
Morten Manzelmann <morten.manzelmann@ministrygroup.de>
2019-05-06 23:25:37 +0200

    formatted code

--- routes/posts.js
+++ routes/posts.js
@@ -8,87 +8,87 @@ var Marabus = require('../models/marabu')
 
 // Add article route
 router.get('/add', (req, res) => res.render('add',{
-	title: 'Add stuff'
+    title: 'Add stuff'
 }))
 
 router.post('/add', function(req,res) {
-	req.checkBody('title', 'Title muss sein').notEmpty();
-	req.checkBody('ort', 'Ort muss sein').notEmpty();
-	req.checkBody('message', 'Nachricht muss sein').notEmpty();
+    req.checkBody('title', 'Title muss sein').notEmpty();
+    req.checkBody('ort', 'Ort muss sein').notEmpty();
+    req.checkBody('message', 'Nachricht muss sein').notEmpty();
 
-	let errors = req.validationErrors();
+    let errors = req.validationErrors();
 
-	if(errors){
-		res.render('add',{
-			title:'Add Post',
-			errors:errors
-		});
-	} else {
-		let marabu = new Marabus;
-		marabu.title = req.body.title;
-		marabu.ort = req.body.ort;
-		marabu.message = req.body.message;
+    if(errors){
+	res.render('add',{
+	    title:'Add Post',
+	    errors:errors
+	});
+    } else {
+	let marabu = new Marabus;
+	marabu.title = req.body.title;
+	marabu.ort = req.body.ort;
+	marabu.message = req.body.message;
 
-		marabu.save(function(err){
-			if (err) {
-				console.log(err);
-				return;
-			}else{
-				req.flash('success', 'Added Post');
-				res.redirect('/');
-			}
-		});
-	}
+	marabu.save(function(err){
+	    if (err) {
+		console.log(err);
+		return;
+	    }else{
+		req.flash('success', 'Added Post');
+		res.redirect('/');
+	    }
+	});
+    }
 
 })
 
 // Update Marabu
 router.post('/edit/:id', function(req,res) {
-	let marabu = {};
-	marabu.title = req.body.title;
-	marabu.ort = req.body.ort;
-	marabu.message = req.body.message;
+    let marabu = {};
+    marabu.title = req.body.title;
+    marabu.ort = req.body.ort;
+    marabu.message = req.body.message;
 
-	let query = {_id:req.params.id}
-	Marabus.update(query, marabu, function(err){
-		if (err) {
-			console.log(err);
-			return;
-		}else{
-			res.redirect('/');
-		}
-	});
+    let query = {_id:req.params.id}
+    Marabus.update(query, marabu, function(err){
+	if (err) {
+	    console.log(err);
+	    return;
+	}else{
+	    res.redirect('/');
+	}
+    });
 })
 
 router.delete('/:id', function(req, res){
-	let query = {_id:req.params.id}
+    let query = {_id:req.params.id}
 
-	Marabus.remove(query, function(err){
-		if (err) {
-			console.log(err);
-		}
-		res.send('Success');
-	})
+    Marabus.remove(query, function(err){
+	if (err) {
+	    console.log(err);
+	}
+	res.send('Success');
+    })
 })
 //Display single post
 router.get('/:id', function(req, res){
 
-	Marabus.findById(req.params.id, function(err, post){
-		res.render('post', {
-			post:post
-		})
+    Marabus.findById(req.params.id, function(err, post){
+	res.render('post', {
+	    post:post
 	})
+    })
 })
 
 //Edit single post
-router.get('/:id', function(req, res){
+router.get('/edit/:id', function(req, res){
 
-	Marabus.findById(req.params.id, function(err, post){
-		res.render('edit_post', {
-			title: 'Edit this Post',
-			post: post
-		})
+    Marabus.findById(req.params.id, function(err, post){
+	res.render('edit_post', {
+	    title: 'Edit this Post',
+	    post: post
 	})
+    })
 })
 
 module.exports = router;
